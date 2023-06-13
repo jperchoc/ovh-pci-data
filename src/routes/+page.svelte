5eb535b658545b3abc0f6d1e36d96a772afa822b
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-06-13 17:23:43 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
     $: user = data.user
 </script>
 
+<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Welcome to PCI-Data project</h1>
+
 {#if user}
     <a href="/project">See your projects</a>
 {:else}
