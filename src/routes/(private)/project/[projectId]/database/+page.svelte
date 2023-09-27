2a798f47376417e9fa8bc7cc293aaad40d6213f4
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-09-27 15:29:40 +0200

    improve form validation
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- src/routes/(private)/project/[projectId]/database/+page.svelte
+++ src/routes/(private)/project/[projectId]/database/+page.svelte
@@ -2,27 +2,24 @@
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 	import { invalidate } from '$app/navigation';
-	import { writable } from 'svelte/store';
 	import ServicesTable from '$components/pages/databases/table/ServicesTable.svelte';
 	import { Card, CardContent, CardHeader } from '$components/ui/card';
 	import { POLLING_INTERVAL } from '$lib/global.constants';
 	import type { ovhapi } from '$types/ovh';
-	import type { ActionData, PageData } from './$types';
+	import type { PageData } from './$types';
 	import * as Dialog from '$components/ui/dialog';
 	import RenameServiceForm from '$components/pages/databases/rename-service/RenameServiceForm.svelte';
 	import Alert from '$components/ui/alert/alert.svelte';
+	import { writable } from 'svelte/store';
+	import { Check, X } from 'lucide-svelte';
 
 	export let data: PageData;
-	export let form: ActionData;
+
+	const pageMessages = writable<string[]>([]);
+	let renameError: string | null;
+	let dialogOpen = false;
 
 	$: services = data.dbServices;
-
-	$: {
-		if (form?.result?.type === 'success') {
-			console.log('yay')
-			$dialogOpen = false;
-		}
-	}
 
 	onMount(() => {
 		const it = setInterval(() => {
@@ -35,19 +32,34 @@
 		};
 	});
 
-	const dialogOpen = writable(false);
 	const openRenameModale = (item: ovhapi.cloud.project.database.Service) => {
 		const serviceToUpdate = services.find(s => s.id === item.id);
 		if (!serviceToUpdate) return;
-		$dialogOpen = true;
+		dialogOpen = true;
 		data.form.data.description = serviceToUpdate.description;
 		data.form.data.id = serviceToUpdate.id;
 		data.form.data.engine = serviceToUpdate.engine;
 	}
+
+
+	function removeMessage(idx: number): void {
+		$pageMessages.splice(idx, 1);
+		$pageMessages = [...$pageMessages];
+	}
 </script>
-{#if form?.result?.type === 'success'}
-	<Alert class="mb-2 bg-green-100 text-green-800" variant="default" >Yay</Alert>
-{/if}
+
+
+{#each $pageMessages as message, idx}
+	<Alert class="mb-2 bg-green-100 text-green-800" variant="default">
+		<div class="flex">
+			<div class="flex-1">
+				<Check class="border rounded-full bg-green-800 text-green-100 inline-block w-4 h-4"/>
+				<span>{message}</span>
+			</div>
+			<button type="button" on:click={() => removeMessage(idx)} aria-label="close"><X class="w-4 h-4"/></button>
+		</div>
+	</Alert>
+{/each}
 <Card>
 	<CardHeader>
 		<h2 class="font-bold text-xl">Databases</h2>
@@ -55,13 +67,25 @@
 	<CardContent class="p-0 pb-4">
 		{#if services}
 			<ServicesTable {services} on:rename-clicked={(e) => openRenameModale(e.detail.service)} />
-			<Dialog.Root open={$dialogOpen} onOpenChange={(state) => { $dialogOpen = state ?? false}}>
+			<Dialog.Root bind:open={dialogOpen}>
 				<Dialog.Content class="sm:max-w-[425px]">
 					<Dialog.Header>
 						<Dialog.Title>Rename service</Dialog.Title>
 						<Dialog.Description>Rename your service here.</Dialog.Description>
 					</Dialog.Header>
-					<RenameServiceForm form={data.form} />
+					{#if renameError}
+						<Alert variant="destructive">{renameError}</Alert>
+					{/if}
+					<RenameServiceForm 
+						form={data.form}
+						on:submit={() => { renameError = null;}}
+						on:success={() => {
+							$pageMessages = [...$pageMessages, 'Your service has been renamed'];
+							dialogOpen = false;
+							renameError = null;
+						}}
+						on:error={(e) => {renameError = `An error occured while renaming your service${e.detail.error ? `: ${e.detail.error.message} (${e.detail.error.status})`:''}`; }}
+					/>
 				</Dialog.Content>
 			</Dialog.Root>
 		{:else}
