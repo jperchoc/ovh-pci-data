062a2cfedebf09a14be78c293ea0871176aa1952
Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>
2023-09-27 16:22:51 +0200

    fix: lint
    Signed-off-by: Jonathan Perchoc <jonathan.perchoc@ovhcloud.com>

--- src/routes/(private)/project/[projectId]/database/+page.ts
+++ src/routes/(private)/project/[projectId]/database/+page.ts
@@ -1,19 +1,25 @@
-import fetchRelog from "$helpers/fetch-relog";
-import type { ovhapi } from "$types/ovh";
-import type { PageLoad } from "./$types";
+import fetchRelog from '$helpers/fetch-relog';
+import type { ovhapi } from '$types/ovh';
+import type { PageLoad } from './$types';
 
-export const load = (async ({ fetch: _fetch, url, params, data}) => {
-    const fetch = (path: string) => fetchRelog(_fetch, url, path);
-    const { projectId } = params;
-    const databasesIdsReq = await fetch(`/api/ovh/cloud/project/${projectId}/database/service`);
-    let dbServices: ovhapi.cloud.project.database.Service[] = [];
-    if (databasesIdsReq.ok) {
-        const dbIds: string[] = await databasesIdsReq.json();
-        dbServices = await Promise.all(dbIds.map(id => fetch(`/api/ovh/cloud/project/${projectId}/database/service/${id}`).then(async (r) => await r.json())))
-    }
+export const load = (async ({ fetch: _fetch, url, params, data }) => {
+	const fetch = (path: string) => fetchRelog(_fetch, url, path);
+	const { projectId } = params;
+	const databasesIdsReq = await fetch(`/api/ovh/cloud/project/${projectId}/database/service`);
+	let dbServices: ovhapi.cloud.project.database.Service[] = [];
+	if (databasesIdsReq.ok) {
+		const dbIds: string[] = await databasesIdsReq.json();
+		dbServices = await Promise.all(
+			dbIds.map((id) =>
+				fetch(`/api/ovh/cloud/project/${projectId}/database/service/${id}`).then(
+					async (r) => await r.json()
+				)
+			)
+		);
+	}
 
-    return {
-        ...data,
-        dbServices
-    }
+	return {
+		...data,
+		dbServices
+	};
 }) satisfies PageLoad;
